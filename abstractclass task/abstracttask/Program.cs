cd41b716995792c0d2c9acc35fa86085b1e1ae81
jabanishvili <85432293+jabanishvili@users.noreply.github.com>
2021-11-12 15:42:16 +0400

    Add files via upload

---
+++ abstractclass task/abstracttask/Program.cs
@@ -0,0 +1,138 @@
+﻿using System;
+
+namespace abstracttask
+{
+
+    public abstract class Human
+    {
+        public string Name { get; set; }
+        public string Lastname { get; set; }
+        public int Age { get; set; }
+
+        public override string ToString()
+        {
+            return ($"{Name}, {Lastname}, {Age}");
+        }
+
+
+    }
+
+    public abstract class Employ : Human
+    {
+        public string JobPositon { get; set; }
+        public double Salary { get; set; }
+
+
+        public abstract void Duties();
+        public abstract void GetBonus();
+
+        public override string ToString()
+        {
+            return ($"{Name}, {Lastname}, {Age}, {JobPositon}, {Salary}");
+        }
+
+
+
+    }
+
+
+
+
+    public class Manager : Employ
+    {
+        public override void Duties()
+        {
+            Console.WriteLine($"agricxva, bonusebis gacema");
+        }
+
+        public override void GetBonus()
+        {
+            Console.WriteLine($"bonus{Salary * 0.5}\n");
+        }
+
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    public class Cashier : Employ
+    {
+        public override void Duties()
+        {
+            Console.WriteLine($"shemosavlis agricxva");
+        }
+        public override void GetBonus()
+        {
+            Console.WriteLine($"bonus{Salary * 0.8}\n");
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+    }
+
+    public class StoreKeeper : Employ
+    {
+        public override void Duties()
+        {
+            Console.WriteLine($"store keeper");
+        }
+        public override void GetBonus()
+        {
+            Console.WriteLine($"bonus{Salary * 0.10}\n");
+        }
+        public override string ToString()
+        {
+            return base.ToString();
+        }
+
+    }
+
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Manager m = new Manager
+            {
+                Name = "ani",
+                Lastname = "jbn",
+                Age = 22,
+                JobPositon = "manager",
+                Salary = 1500
+            };
+            Console.WriteLine(m);
+            m.Duties();
+            m.GetBonus();
+
+
+
+            Cashier c = new Cashier
+            {
+                Name = "john",
+                Lastname = "dou",
+                Age = 20,
+                JobPositon = "cashier",
+                Salary = 2000
+            };
+            Console.WriteLine(c);
+            c.Duties();
+            c.GetBonus();
+
+
+
+            StoreKeeper st = new StoreKeeper
+            {
+                Name = "john",
+                Lastname = "snow",
+                Age = 26,
+                JobPositon = "storekeeper",
+                Salary = 3000
+            };
+            Console.WriteLine(st);
+            st.Duties();
+            st.GetBonus();
+        }
+    }
+}
---
+++ abstractclass task2/abstractclass/Program.cs
@@ -0,0 +1,168 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Lesson11SM011
+{
+    public abstract class Human
+    {
+        string _fname;
+        string _lname;
+        DateTime _bd;
+
+        public Human(string _fname, string _lname, DateTime _bd)
+        {
+            this._fname = _fname;
+            this._lname = _lname;
+            this._bd = _bd;
+        }
+        public abstract void Think();
+        public override string ToString()
+        {
+            return $"{_fname}, {_lname}, {_bd.ToShortDateString()}";
+        }
+
+    }
+    public abstract class Learner : Human
+    {
+        string _saswavlebeli;
+        public Learner(string saxeli, string gvari, DateTime dt, string saswavlebeli) :
+            base(saxeli, gvari, dt)
+        {
+            _saswavlebeli = saswavlebeli;
+        }
+        public abstract void Study();
+        public override string ToString()
+        {
+            return base.ToString() + $" {_saswavlebeli}";
+        }
+    }
+    public class Student : Learner
+    {
+        string _groupNumber;
+        public Student(string saxeli, string gvari, DateTime dt, string saswavlebeli, string group) :
+            base(saxeli, gvari, dt, saswavlebeli)
+        {
+            _groupNumber = group;
+        }
+        public override void Study()
+        {
+            Console.WriteLine("me vswavlob c#-s saswavlebelshi");
+        }
+
+        public override void Think()
+        {
+            Console.WriteLine("me vfiqrob rogorc studenti");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" {_groupNumber}";
+        }
+    }
+    public class SchoolChild : Learner
+    {
+        string _className;
+        public SchoolChild(string saxeli, string gvari, DateTime dt, string saswavlebeli, string _className) :
+            base(saxeli, gvari, dt, saswavlebeli)
+        {
+            this._className = _className;
+        }
+        public override void Study()
+        {
+            Console.WriteLine("me vswavlob skolashi qartuls, matematikas da assh...");
+        }
+
+        public override void Think()
+        {
+            Console.WriteLine("me vfiqrob rogorc skolis moswavle");
+        }
+        public override string ToString()
+        {
+            return base.ToString() + $" {_className}";
+        }
+    }
+
+
+    public abstract class Adamiani
+    {
+        public string Saxeli { get; set; }
+        public string Gvari { get; set; }
+        public int Asaki { get; set; }
+
+        public override string ToString()
+        {
+            return $"{Saxeli}, {Gvari}, {Asaki}";
+        }
+    }
+
+    public abstract class Samsaxuri : Adamiani
+    {
+        public string Pozicia { get; set; }
+        public int Xelfasi { get; set; }
+
+        public abstract void SamushaosAghwera();
+        public abstract void Bonusi();
+        public override string ToString()
+        {
+            return base.ToString() + $", {Pozicia}, {Xelfasi}";
+        }
+    }
+    public class Manager : Samsaxuri
+    {
+        public override void Bonusi()
+        {
+            Console.WriteLine($"{Xelfasi * 0.05}");
+        }
+
+        public override void SamushaosAghwera()
+        {
+            Console.WriteLine("9dan 6 saatamde samsaxurshi yofna!");
+        }
+    }
+    public class Molare : Samsaxuri
+    {
+        public override void Bonusi()
+        {
+            Console.WriteLine($"{Xelfasi * 0.05}");
+        }
+
+        public override void SamushaosAghwera()
+        {
+            Console.WriteLine("fulis datvla da gacema");
+        }
+    }
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Learner[] learners =
+            //{
+            //    new Student("John","Doe",DateTime.Now,"Harvard university","SM011"),
+            //    new SchoolChild("Jane","Doe",new DateTime(2007,05,12),"31 sajaro skola","A")
+            //};
+            //foreach (var item in learners)
+            //{
+            //    Console.WriteLine(item);
+            //    item.Study();
+            //    item.Think();
+            //}
+
+            Samsaxuri[] tanamshromlebi =
+            {
+                new Manager{Saxeli="ani",Gvari = "jabannishvili",Asaki=25,Pozicia="IT Manager",Xelfasi =1200},
+                new Manager{Saxeli="Giorgi",Gvari = "Giorgadze",Asaki=21,Pozicia="Sales Manager",Xelfasi =1200},
+                new Molare{Saxeli="Parmen",Gvari = "Jokovic",Asaki=21,Pozicia="Molare operatori",Xelfasi =1200}
+            };
+            foreach (var item in tanamshromlebi)
+            {
+                Console.WriteLine(item);
+                item.SamushaosAghwera();
+                item.Bonusi();
+            }
+
+            Console.Read();
+        }
+    }
+}
